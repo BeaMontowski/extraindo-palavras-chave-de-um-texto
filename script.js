da5ec9d8b86025a903cc06d5cdaafe6270868c37
Beatriz <beatriz.montowski@escola.pr.gov.br>
2025-06-28 16:15:55 -0300

    Removendo Caracteres Especiais

--- script.js
+++ script.js
@@ -1,9 +1,4 @@
-const botaoMostraPalavras = document.querySelector("#botao-palavrachave"); 
-
-botaoMostraPalavras.addEventListener("click", mostraPalavrasChave); 
-
-function mostraPalavrasChave() { 
-  const botaoMostraPalavras = document.querySelector("#botao-palavrachave");
+const botaoMostraPalavras = document.querySelector("#botao-palavrachave");
 
 botaoMostraPalavras.addEventListener("click", mostraPalavrasChave);
 
@@ -12,8 +7,13 @@ function mostraPalavrasChave() {
 
   const campoResultado = document.querySelector("#resultado-palavrachave");
 
-  const palavras = texto.split(" ");
+  const palavrasChave = processaTexto(texto);
 
-  campoResultado.textContent = palavras.join(", ");
+  campoResultado.textContent = palavrasChave.join(", ");
 }
-} 
+
+function processaTexto(texto) {
+  let palavras = texto.split(/\P{L}+/u);
+
+  return palavras;
+}
